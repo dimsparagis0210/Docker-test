4febfe9699f24f4f733c2aad18ba3609c8157018
Dimitris Sparagis <96797730+dimsparagis0210@users.noreply.github.com>
2025-05-22 14:11:30 +0000

    Test

---
+++ demo 2/src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String getHello() {
+        return "Hello1";
+    }
+
+    @GetMapping("hello2")
+    public String getHello2() {
+        return "Hello2";
+    }
+
+}
